c638a09d097c33987734de7921a085367805789d
songfei <784816716@qq.com>
2020-12-25 11:44:37 +0800

    springboot启动器监听器

--- SpringbootApp/src/main/java/com/sf/SpringbootApp.java
+++ SpringbootApp/src/main/java/com/sf/SpringbootApp.java
@@ -3,21 +3,18 @@ package com.sf;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class SpringbootApp {
+public class SpringbootApp extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApp.class, args);
-    }
-
-    @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
     }
 
 }
