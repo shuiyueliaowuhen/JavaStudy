e50cb9c19d9055e90f64b1e363bcb833ee12dec3
songfei <784816716@qq.com>
2021-01-08 14:05:50 +0800

    spring-事物

---
+++ SpringStudy/src/test/java/Cap11Test.java
@@ -0,0 +1,20 @@
+
+import com.sf.cap11.config.Cap11MainConfig;
+import com.sf.cap11.service.OrderService;
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+public class Cap11Test {
+	@Test
+	public void test01(){
+		AnnotationConfigApplicationContext app = new AnnotationConfigApplicationContext(Cap11MainConfig.class);
+		
+		OrderService bean = app.getBean(OrderService.class);
+		bean.addOrder();
+		
+		app.close();
+	
+		
+	}
+}
